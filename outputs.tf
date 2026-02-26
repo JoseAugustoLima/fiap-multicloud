cf5aba271d3cd2a66a2ac19109a11f2bbe8195d5
joseaugustolima <66323877+JoseAugustoLima@users.noreply.github.com>
2026-02-26 18:28:08 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-#output "instance_public_ip" {
-#  description = "Public IP address of the EC2 instance"
-#  value       = aws_instance.demo_server.public_ip
-#}
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.demo_server.public_ip
+}
 
 output "ip" {
   description = "Public IP address of the GCE instance"
