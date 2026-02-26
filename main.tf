d4a800566d26b4c2adaca59b597e7c1e0ab90f18
joseaugustolima <66323877+JoseAugustoLima@users.noreply.github.com>
2026-02-26 18:36:53 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ provider "google" {
 # AWS Resource #
 resource "aws_instance" "demo_server" {
   ami           = "ami-02e136e904f3da870"
-  instance_type = "t2.nano"
+  instance_type = "t2.medium"
   tags = {
     Name = "Demo-TFC"
   }
