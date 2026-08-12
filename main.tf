b64cb1b12857c25ece423ee5fae709e8aea748c2
joseaugustolima <66323877+JoseAugustoLima@users.noreply.github.com>
2026-08-12 19:05:29 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_instance" "vm_instance" {
   name         = "demo-tfc"
   machine_type = "e2-micro"
-  zone         = "us-east1-a"
+  zone         = "us-central1-b"
 
   boot_disk {
     initialize_params {
